e257c7f2df249ff7cb1b25260f07552474385437
Nikeshhh <WityazKajit@gmail.com>
2024-03-29 09:34:53 +0300

    fixed load fixtures script

--- load_fixtures.sh
+++ load_fixtures.sh
@@ -1,2 +1,2 @@
-python manage.py loaddata fixtures/cars.json --app delivery.DeliveryCar &&
-python manage.py loaddata fixtures/locations.json --app delivery.Location
+python manage.py loaddata fixtures/locations.json --app delivery.Location &&
+python manage.py loaddata fixtures/cars.json --app delivery.DeliveryCar
